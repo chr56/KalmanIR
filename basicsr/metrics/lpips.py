50e32cc5d05bf7c0880df63d55c63c50e765a4e6
chr56 <->
2025-07-27 17:40:33 +0800

    metric: add LPIPS & NIQE

---
+++ basicsr/metrics/lpips.py
@@ -0,0 +1,33 @@
+import torch
+import lpips
+import numpy as np
+from basicsr.metrics.metric_util import reorder_image
+from basicsr.utils.registry import METRIC_REGISTRY
+
+
+@METRIC_REGISTRY.register()
+def calculate_lpips(img, img2, input_order='HWC', model='alex', **kwargs):
+    """Calculate LPIPS (Learned Perceptual Image Patch Similarity).
+
+    Args:
+        img (ndarray): Ground-truth image. [0, 255], uint8
+        img2 (ndarray): Predicted image. [0, 255], uint8
+        input_order (str): 'HWC' or 'CHW'. Default: 'HWC'.
+        model (str): LPIPS model type: 'alex', 'vgg', or 'squeeze'
+
+    Returns:
+        float: LPIPS score.
+    """
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    loss_fn = lpips.LPIPS(net=model).to(device)
+
+    # Reorder and normalize
+    img = reorder_image(img, input_order=input_order)
+    img2 = reorder_image(img2, input_order=input_order)
+    img = img.astype(np.float32) / 255. * 2 - 1
+    img2 = img2.astype(np.float32) / 255. * 2 - 1
+
+    img = torch.from_numpy(np.transpose(img, (2, 0, 1))).unsqueeze(0).to(device)
+    img2 = torch.from_numpy(np.transpose(img2, (2, 0, 1))).unsqueeze(0).to(device)
+
+    return loss_fn(img, img2).item()
