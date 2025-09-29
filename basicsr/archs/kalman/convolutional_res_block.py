ceb52292e78b026862bc72cff9d4beea9f442c4b
chr56 <->
2025-09-29 10:34:53 +0800

    arch: add `ConvolutionalResBlockLayerNorm`

--- basicsr/archs/kalman/convolutional_res_block.py
+++ basicsr/archs/kalman/convolutional_res_block.py
@@ -1,3 +1,5 @@
+from typing import Callable, Literal, Union
+
 import torch
 from torch import nn
 
@@ -44,3 +46,63 @@ class ConvolutionalResBlock(nn.Module):
 @torch.jit.script
 def swish(x):
     return x * torch.sigmoid(x)
+
+
+ActivationFunction = Literal['relu', 'leaky_relu', 'silu', 'sigmoid']
+
+
+def get_activation_function(activation_type, required: bool = True, **kwargs):
+    if activation_type == 'relu':
+        return nn.ReLU(inplace=True)
+    elif activation_type == 'leaky_relu':
+        return nn.LeakyReLU(inplace=True, **kwargs)
+    elif activation_type == 'silu':
+        return nn.SiLU(inplace=True)
+    elif activation_type == 'sigmoid':
+        return nn.Sigmoid()
+    else:
+        if required:
+            raise NotImplementedError(f"activation type `{activation_type}` is unimplemented")
+        else:
+            return nn.Identity()
+
+
+class ConvolutionalResBlockLayerNorm(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            out_channels: int = -1,
+            activation_type: ActivationFunction = 'relu',
+            kernel_size: int = 3
+    ):
+        super(ConvolutionalResBlockLayerNorm, self).__init__()
+
+        out_channels = out_channels if out_channels > 0 else channels
+
+        self.channels = channels
+        self.out_channels = out_channels
+
+        from .utils import LayerNorm2d
+        self.norm1 = LayerNorm2d(channels, eps=1e-6, elementwise_affine=True)
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=kernel_size, padding='same', stride=1)
+        self.norm2 = LayerNorm2d(channels, eps=1e-6, elementwise_affine=True)
+        self.conv2 = nn.Conv2d(channels, out_channels, kernel_size=kernel_size, padding='same', stride=1)
+
+        if channels != out_channels:
+            self.conv_residual = nn.Conv2d(channels, out_channels, kernel_size=1, padding='same')
+
+        self.activation = get_activation_function(activation_type)
+
+    def forward(self, x_in):
+        x = x_in
+        x = self.norm1(x)
+        x = self.activation(x)
+        x = self.conv1(x)
+        x = self.norm2(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+
+        if self.channels != self.out_channels:
+            x_in = self.conv_residual(x_in)
+
+        return x + x_in
