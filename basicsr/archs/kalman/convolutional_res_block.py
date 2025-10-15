157bae15a3ab3749d4b40538f00065d7af5b0c91
chr56 <->
2025-10-15 23:57:59 +0800

    arch: add `ConvolutionalResBlockGroupNorm`

--- basicsr/archs/kalman/convolutional_res_block.py
+++ basicsr/archs/kalman/convolutional_res_block.py
@@ -106,3 +106,44 @@ class ConvolutionalResBlockLayerNorm(nn.Module):
             x_in = self.conv_residual(x_in)
 
         return x + x_in
+
+
+class ConvolutionalResBlockGroupNorm(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            norm_group: int = 1,
+            out_channels: int = -1,
+            activation_type: ActivationFunction = 'relu',
+            kernel_size: int = 3
+    ):
+        super(ConvolutionalResBlockGroupNorm, self).__init__()
+
+        out_channels = out_channels if out_channels > 0 else channels
+
+        self.channels = channels
+        self.out_channels = out_channels
+
+        self.norm1 = nn.GroupNorm(norm_group, channels, eps=1e-6)
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=kernel_size, padding='same', stride=1)
+        self.norm2 = nn.GroupNorm(norm_group, channels, eps=1e-6)
+        self.conv2 = nn.Conv2d(channels, out_channels, kernel_size=kernel_size, padding='same', stride=1)
+
+        if channels != out_channels:
+            self.conv_residual = nn.Conv2d(channels, out_channels, kernel_size=1, padding='same')
+
+        self.activation = get_activation_function(activation_type)
+
+    def forward(self, x_in):
+        x = x_in
+        x = self.norm1(x)
+        x = self.activation(x)
+        x = self.conv1(x)
+        x = self.norm2(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+
+        if self.channels != self.out_channels:
+            x_in = self.conv_residual(x_in)
+
+        return x + x_in
