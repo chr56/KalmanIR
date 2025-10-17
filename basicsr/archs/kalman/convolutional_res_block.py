73409a447c4a1a9a116eedc2fa75f07afb042beb
chr56 <->
2025-10-17 14:48:06 +0800

    arch: extract `_BaseConvolutionalResBlock`

--- basicsr/archs/kalman/convolutional_res_block.py
+++ basicsr/archs/kalman/convolutional_res_block.py
@@ -67,25 +67,28 @@ def get_activation_function(activation_type, required: bool = True, **kwargs):
             return nn.Identity()
 
 
-class ConvolutionalResBlockLayerNorm(nn.Module):
+class _BaseConvolutionalResBlock(nn.Module):
+
     def __init__(
             self,
+            norm_layer1: nn.Module,
+            norm_layer2: nn.Module,
             channels: int,
             out_channels: int = -1,
             activation_type: ActivationFunction = 'relu',
             kernel_size: int = 3
     ):
-        super(ConvolutionalResBlockLayerNorm, self).__init__()
+        super(_BaseConvolutionalResBlock, self).__init__()
 
         out_channels = out_channels if out_channels > 0 else channels
 
         self.channels = channels
         self.out_channels = out_channels
 
-        from .utils import LayerNorm2d
-        self.norm1 = LayerNorm2d(channels, eps=1e-6, elementwise_affine=True)
+        self.norm1 = norm_layer1
         self.conv1 = nn.Conv2d(channels, channels, kernel_size=kernel_size, padding='same', stride=1)
-        self.norm2 = LayerNorm2d(channels, eps=1e-6, elementwise_affine=True)
+
+        self.norm2 = norm_layer2
         self.conv2 = nn.Conv2d(channels, out_channels, kernel_size=kernel_size, padding='same', stride=1)
 
         if channels != out_channels:
@@ -108,7 +111,31 @@ class ConvolutionalResBlockLayerNorm(nn.Module):
         return x + x_in
 
 
-class ConvolutionalResBlockGroupNorm(nn.Module):
+class ConvolutionalResBlockLayerNorm(_BaseConvolutionalResBlock):
+
+    def __init__(
+            self,
+            channels: int,
+            out_channels: int = -1,
+            activation_type: ActivationFunction = 'relu',
+            kernel_size: int = 3
+    ):
+        from .utils import LayerNorm2d
+        norm1 = LayerNorm2d(channels, eps=1e-6, elementwise_affine=True)
+        norm2 = LayerNorm2d(channels, eps=1e-6, elementwise_affine=True)
+
+        super(ConvolutionalResBlockLayerNorm, self).__init__(
+            norm_layer1=norm1,
+            norm_layer2=norm2,
+            channels=channels,
+            out_channels=out_channels,
+            activation_type=activation_type,
+            kernel_size=kernel_size
+        )
+
+
+class ConvolutionalResBlockGroupNorm(_BaseConvolutionalResBlock):
+
     def __init__(
             self,
             channels: int,
@@ -117,33 +144,14 @@ class ConvolutionalResBlockGroupNorm(nn.Module):
             activation_type: ActivationFunction = 'relu',
             kernel_size: int = 3
     ):
-        super(ConvolutionalResBlockGroupNorm, self).__init__()
+        norm1 = nn.GroupNorm(norm_group, channels, eps=1e-6)
+        norm2 = nn.GroupNorm(norm_group, channels, eps=1e-6)
 
-        out_channels = out_channels if out_channels > 0 else channels
-
-        self.channels = channels
-        self.out_channels = out_channels
-
-        self.norm1 = nn.GroupNorm(norm_group, channels, eps=1e-6)
-        self.conv1 = nn.Conv2d(channels, channels, kernel_size=kernel_size, padding='same', stride=1)
-        self.norm2 = nn.GroupNorm(norm_group, channels, eps=1e-6)
-        self.conv2 = nn.Conv2d(channels, out_channels, kernel_size=kernel_size, padding='same', stride=1)
-
-        if channels != out_channels:
-            self.conv_residual = nn.Conv2d(channels, out_channels, kernel_size=1, padding='same')
-
-        self.activation = get_activation_function(activation_type)
-
-    def forward(self, x_in):
-        x = x_in
-        x = self.norm1(x)
-        x = self.activation(x)
-        x = self.conv1(x)
-        x = self.norm2(x)
-        x = self.activation(x)
-        x = self.conv2(x)
-
-        if self.channels != self.out_channels:
-            x_in = self.conv_residual(x_in)
-
-        return x + x_in
+        super(ConvolutionalResBlockGroupNorm, self).__init__(
+            norm_layer1=norm1,
+            norm_layer2=norm2,
+            channels=channels,
+            out_channels=out_channels,
+            activation_type=activation_type,
+            kernel_size=kernel_size
+        )
