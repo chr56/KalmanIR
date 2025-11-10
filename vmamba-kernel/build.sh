f8f97be5b63b99441fada50ef335a96e470312dc
chr56 <->
2025-11-11 00:50:52 +0800

    v2dmamba: fix build

--- vmamba-kernel/build.sh
+++ vmamba-kernel/build.sh
@@ -7,7 +7,7 @@ fi
 mkdir build
 
 PYTHON_ENV="$CONDA_PREFIX"
-OUTPUT_DIR="../basicsr/archs/vmamba_2d/v2dmamba_scan"
+OUTPUT_DIR="../../basicsr/archs/vmamba_2d/v2dmamba_scan"
 
 cmake -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR="$PYTHON_ENV" -DCUDA_ARCHS="89;75;80" -DOUTPUT_DIRECTORY="$OUTPUT_DIR" -B build
 
