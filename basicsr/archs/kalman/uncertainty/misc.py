dd03143da58c40f182da1a808251978bdcd8d364
chr56 <->
2025-11-14 15:46:40 +0800

    arch: remove legacy KalmanIR & DichotomyIR

--- basicsr/archs/kalman/uncertainty/misc.py
+++
@@ -1,7 +0,0 @@
-import torch
-from torch import nn
-
-
-class DummyUncertaintyEstimator(nn.Module):
-    def forward(self, image_sequence: torch.Tensor, difficult_zone: torch.Tensor, sigma: torch.Tensor):
-        return difficult_zone
