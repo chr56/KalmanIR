b1cbb5f75209ee38af3cca047a7bd5f72b79a18e
chr56 <->
2025-11-14 20:28:59 +0800

    arch: add new models

---
+++ basicsr/archs/MambaIRV2_multi_branch_arch.py
@@ -0,0 +1,272 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from basicsr.archs.arch_util import init_weights, trunc_normal_
+from basicsr.utils.registry import ARCH_REGISTRY
+
+
+@ARCH_REGISTRY.register()
+class MultiBranchMambaIRV2(nn.Module):
+    def __init__(self,
+                 branch=3,
+                 upscale=4,
+                 channel=3,
+                 norm_image=False,
+                 img_size=64,
+                 patch_size=1,
+                 embed_dim=48,
+                 d_state=8,
+                 depths=(6, 6, 6, 6,),
+                 num_heads=(4, 4, 4, 4,),
+                 window_size=16,
+                 inner_rank=32,
+                 num_tokens=64,
+                 convffn_kernel_size=5,
+                 mlp_ratio=2.,
+                 qkv_bias=True,
+                 norm_layer=nn.LayerNorm,
+                 ape=False,
+                 patch_norm=True,
+                 use_checkpoint=False,
+                 img_range=1.,
+                 upsampler='',
+                 resi_connection='1conv',
+                 **kwargs):
+        from .components_MambaIRv2 import (
+            PatchEmbed, PatchUnEmbed, Upsample, UpsampleOneStep,
+        )
+        from .components_MambaIRv2 import ASSB
+        from .components_MambaIRv2 import window_partition
+
+        super().__init__()
+        num_in_ch = channel
+        num_out_ch = channel * branch
+        num_feat = 64
+        self.branch = branch
+
+        self.norm_image = norm_image
+        self.img_range = img_range
+
+        if self.norm_image:
+            if channel == 3:
+                self.mean = torch.Tensor((0.4488, 0.4371, 0.4040)).view(1, 3, 1, 1)  # RGB
+            else:
+                self.mean = torch.zeros(1, 1, 1, 1)  # GREY
+        else:
+            self.mean = None
+
+        self.upscale = upscale
+        self.upsampler = upsampler
+
+        self.window_partition_fn = window_partition
+
+        # ------------------------- 1, shallow feature extraction ------------------------- #
+        self.conv_first = nn.Conv2d(num_in_ch, embed_dim, 3, 1, 1)
+
+        # ------------------------- 2, deep feature extraction ------------------------- #
+        self.num_layers = len(depths)
+        self.embed_dim = embed_dim
+        self.ape = ape
+        self.patch_norm = patch_norm
+        self.num_features = embed_dim
+        self.mlp_ratio = mlp_ratio
+        self.window_size = window_size
+
+        # split image into non-overlapping patches
+        self.patch_embed = PatchEmbed(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=embed_dim,
+            embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+        num_patches = self.patch_embed.num_patches
+        patches_resolution = self.patch_embed.patches_resolution
+        self.patches_resolution = patches_resolution
+
+        # merge non-overlapping patches into image
+        self.patch_unembed = PatchUnEmbed(
+            img_size=img_size,
+            patch_size=patch_size,
+            in_chans=embed_dim,
+            embed_dim=embed_dim,
+            norm_layer=norm_layer if self.patch_norm else None)
+
+        # absolute position embedding
+        if self.ape:
+            self.absolute_pos_embed = nn.Parameter(torch.zeros(1, num_patches, embed_dim))
+            trunc_normal_(self.absolute_pos_embed, std=.02)
+
+        # relative position index
+        relative_position_index_SA = self.calculate_rpi_sa()
+        self.register_buffer('relative_position_index_SA', relative_position_index_SA)
+
+        self.layers = nn.ModuleList()
+        for i_layer in range(self.num_layers):
+            layer = ASSB(
+                dim=embed_dim,
+                d_state=d_state,
+                idx=i_layer,
+                input_resolution=(patches_resolution[0], patches_resolution[1]),
+                depth=depths[i_layer],
+                num_heads=num_heads[i_layer],
+                window_size=window_size,
+                inner_rank=inner_rank,
+                num_tokens=num_tokens,
+                convffn_kernel_size=convffn_kernel_size,
+                mlp_ratio=self.mlp_ratio,
+                qkv_bias=qkv_bias,
+                norm_layer=norm_layer,
+                downsample=None,
+                use_checkpoint=use_checkpoint,
+                img_size=img_size,
+                patch_size=patch_size,
+                resi_connection=resi_connection,
+            )
+            self.layers.append(layer)
+        self.norm = norm_layer(self.num_features)
+
+        # build the last conv layer in deep feature extraction
+        if resi_connection == '1conv':
+            self.conv_after_body = nn.Conv2d(embed_dim, embed_dim, 3, 1, 1)
+        elif resi_connection == '3conv':
+            # to save parameters and memory
+            self.conv_after_body = nn.Sequential(
+                nn.Conv2d(embed_dim, embed_dim // 4, 3, 1, 1), nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                nn.Conv2d(embed_dim // 4, embed_dim // 4, 1, 1, 0), nn.LeakyReLU(negative_slope=0.2, inplace=True),
+                nn.Conv2d(embed_dim // 4, embed_dim, 3, 1, 1))
+
+        # ------------------------- 3, high quality image reconstruction ------------------------- #
+        if self.upsampler == 'pixelshuffle':
+            # for classical SR
+            self.conv_before_upsample = nn.Sequential(
+                nn.Conv2d(embed_dim, num_feat, 3, 1, 1), nn.LeakyReLU(inplace=True))
+            self.upsample = Upsample(upscale, num_feat)
+            self.conv_last = nn.Conv2d(num_feat, num_out_ch, 3, 1, 1)
+        elif self.upsampler == 'pixelshuffledirect':
+            # for lightweight SR (to save parameters)
+            self.upsample = UpsampleOneStep(upscale, embed_dim, num_out_ch,
+                                            (patches_resolution[0], patches_resolution[1]))
+        elif self.upsampler == 'nearest+conv':
+            # for real-world SR (less artifacts)
+            assert self.upscale == 4, 'only support x4 now.'
+            self.conv_before_upsample = nn.Sequential(
+                nn.Conv2d(embed_dim, num_feat, 3, 1, 1), nn.LeakyReLU(inplace=True))
+            self.conv_up1 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+            self.conv_up2 = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+            self.conv_hr = nn.Conv2d(num_feat, num_feat, 3, 1, 1)
+            self.conv_last = nn.Conv2d(num_feat, num_out_ch, 3, 1, 1)
+            self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
+        else:
+            # for image denoising and JPEG compression artifact reduction
+            self.conv_last = nn.Conv2d(embed_dim, num_out_ch, 3, 1, 1)
+
+        self.apply(init_weights)
+
+    @torch.jit.ignore
+    def no_weight_decay(self):
+        return {'absolute_pos_embed'}
+
+    @torch.jit.ignore
+    def no_weight_decay_keywords(self):
+        return {'relative_position_bias_table'}
+
+    def forward_features(self, x, params):
+        x_size = (x.shape[2], x.shape[3])
+        x = self.patch_embed(x)
+        if self.ape:
+            x = x + self.absolute_pos_embed
+
+        for layer in self.layers:
+            x = layer(x, x_size, params)
+
+        x = self.norm(x)  # b seq_len c
+        x = self.patch_unembed(x, x_size)
+
+        return x
+
+    def calculate_rpi_sa(self):
+        coords_h = torch.arange(self.window_size)
+        coords_w = torch.arange(self.window_size)
+        coords = torch.stack(torch.meshgrid([coords_h, coords_w]))  # 2, Wh, Ww
+        coords_flatten = torch.flatten(coords, 1)  # 2, Wh*Ww
+        relative_coords = coords_flatten[:, :, None] - coords_flatten[:, None, :]  # 2, Wh*Ww, Wh*Ww
+        relative_coords = relative_coords.permute(1, 2, 0).contiguous()  # Wh*Ww, Wh*Ww, 2
+        relative_coords[:, :, 0] += self.window_size - 1  # shift to start from 0
+        relative_coords[:, :, 1] += self.window_size - 1
+        relative_coords[:, :, 0] *= 2 * self.window_size - 1
+        relative_position_index = relative_coords.sum(-1)  # Wh*Ww, Wh*Ww
+        return relative_position_index
+
+    def calculate_mask(self, x_size):
+        h, w = x_size
+        img_mask = torch.zeros((1, h, w, 1))  # 1 h w 1
+        h_slices = (slice(0, -self.window_size), slice(-self.window_size,
+                                                       -(self.window_size // 2)), slice(-(self.window_size // 2), None))
+        w_slices = (slice(0, -self.window_size), slice(-self.window_size,
+                                                       -(self.window_size // 2)), slice(-(self.window_size // 2), None))
+        cnt = 0
+        for h in h_slices:
+            for w in w_slices:
+                img_mask[:, h, w, :] = cnt
+                cnt += 1
+
+        mask_windows = self.window_partition_fn(img_mask, self.window_size)  # nw, window_size, window_size, 1
+        mask_windows = mask_windows.view(-1, self.window_size * self.window_size)
+        attn_mask = mask_windows.unsqueeze(1) - mask_windows.unsqueeze(2)
+        attn_mask = attn_mask.masked_fill(attn_mask != 0, float(-100.0)).masked_fill(attn_mask == 0, float(0.0))
+
+        return attn_mask
+
+    def forward(self, x):
+        # padding
+        h_ori, w_ori = x.size()[-2], x.size()[-1]
+        mod = self.window_size
+        h_pad = ((h_ori + mod - 1) // mod) * mod - h_ori
+        w_pad = ((w_ori + mod - 1) // mod) * mod - w_ori
+        h, w = h_ori + h_pad, w_ori + w_pad
+        x = torch.cat([x, torch.flip(x, [2])], 2)[:, :, :h, :]
+        x = torch.cat([x, torch.flip(x, [3])], 3)[:, :, :, :w]
+
+        if self.norm_image:
+            self.mean = self.mean.type_as(x)
+            x = (x - self.mean) * self.img_range
+
+        attn_mask = self.calculate_mask([h, w]).to(x.device)
+        params = {'attn_mask': attn_mask, 'rpi_sa': self.relative_position_index_SA}
+
+        if self.upsampler == 'pixelshuffle':
+            # for classical SR
+            x = self.conv_first(x)
+            x = self.conv_after_body(self.forward_features(x, params)) + x
+            x = self.conv_before_upsample(x)
+            x = self.conv_last(self.upsample(x))
+        elif self.upsampler == 'pixelshuffledirect':
+            # for lightweight SR
+            x = self.conv_first(x)
+            x = self.conv_after_body(self.forward_features(x, params)) + x
+            x = self.upsample(x)
+        elif self.upsampler == 'nearest+conv':
+            # for real-world SR
+            x = self.conv_first(x)
+            x = self.conv_after_body(self.forward_features(x, params)) + x
+            x = self.conv_before_upsample(x)
+            x = self.lrelu(self.conv_up1(torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')))
+            x = self.lrelu(self.conv_up2(torch.nn.functional.interpolate(x, scale_factor=2, mode='nearest')))
+            x = self.conv_last(self.lrelu(self.conv_hr(x)))
+            x = x.contiguous()
+        else:
+            # for image denoising and JPEG compression artifact reduction
+            x_in = x
+            x = self.conv_first(x)
+            x = self.conv_after_body(self.forward_features(x, params)) + x
+            x = self.conv_last(x) + x_in.repeat(1, self.branch, 1, 1)
+            x = x.contiguous()
+
+        if self.norm_image:
+            x = x / self.img_range + self.mean
+
+        # unpadding
+        x = x[..., :h_ori * self.upscale, :w_ori * self.upscale]
+
+        return x
