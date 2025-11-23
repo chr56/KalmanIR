423aeb01f51576e9d09693f640fdf8e0ac9daf90
chr56 <->
2025-11-23 14:53:21 +0800

    arch: `DifficultZoneEstimatorV3`s

--- basicsr/archs/modules/DifficultZoneEstimator_module.py
+++ basicsr/archs/modules/DifficultZoneEstimator_module.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 
 import torch
 from torch import nn
@@ -139,3 +139,221 @@ class DifficultZoneEstimatorV2s(nn.Module):
         difficult_zone = x * self.merge_ratio + avg_ae_differences
 
         return torch.tanh(difficult_zone)
+
+
+@MODULES_REGISTRY.register()
+class DifficultZoneEstimatorV3base(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            num_images: int,
+            merge_ratio: float = 1.0,
+            final_transform: str = 'none',
+    ):
+        super().__init__()
+
+        self.channels = channels
+        self.num_images = num_images
+
+        self.merge_ratio = merge_ratio
+        self.final_transform = final_transform
+
+        from .residual_conv_block import ResidualConvBlock
+        diff_metric_methods = 2
+        ch_layer_0 = channels * num_images * diff_metric_methods
+        ch_layer_1 = channels * num_images
+        ch_layer_2 = channels
+        self.conv_block_0 = ResidualConvBlock(
+            in_channels=ch_layer_0, out_channels=ch_layer_0, num_layers=3,
+            activation_type='silu', norm_type='group', norm_group=num_images * diff_metric_methods,
+        )
+        self.conv_block_1 = ResidualConvBlock(
+            in_channels=ch_layer_0, out_channels=ch_layer_1, num_layers=2,
+            activation_type='silu', norm_type='layer',
+        )
+        self.conv_block_2 = ResidualConvBlock(
+            in_channels=ch_layer_1, out_channels=ch_layer_2, num_layers=2,
+            activation_type='sigmoid', norm_type='layer',
+        )
+
+    def forward(self, images: List[torch.Tensor]) -> torch.Tensor:
+        """
+        :param images: image sequence, shape [B, L, C, H, W]
+        :return: Difficult Zone, shape [B, C, H, W]
+        """
+
+        ##################################
+        ae_differences = []
+        bce_differences = []
+        for i in range(self.num_images):
+            ae_differences.append(cal_ae(images[:, i, ...], images[:, i - 1, ...]))
+            bce_differences.append(cal_bce(images[:, i, ...], images[:, i - 1, ...]))
+
+        ##################################
+
+        avg_ae_differences = torch.mean(torch.stack(ae_differences), dim=0)
+
+        ae_differences = torch.cat(ae_differences, dim=1)
+        bce_differences = torch.cat(bce_differences, dim=1)
+        differences = torch.cat((ae_differences, bce_differences), dim=1)
+
+        ##################################
+
+        x = differences
+        x = self.conv_block_0(x)
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+
+        difficult_zone = x * self.merge_ratio + avg_ae_differences
+
+        if self.final_transform == 'tanh':
+            return torch.tanh(difficult_zone)
+        elif self.final_transform == 'sigmoid':
+            return torch.sigmoid(difficult_zone)
+        else:
+            return difficult_zone
+
+
+@MODULES_REGISTRY.register()
+class DifficultZoneEstimatorV3w(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            num_images: int,
+            merge_ratio: float = 1.0,
+            final_transform: str = 'none',
+    ):
+        super().__init__()
+
+        self.channels = channels
+        self.num_images = num_images
+
+        self.merge_ratio = merge_ratio
+        self.final_transform = final_transform
+
+        from .residual_conv_block import ResidualConvBlock
+        ch_layer_1 = channels * num_images
+        ch_layer_2 = channels
+        self.conv_block_0 = ResidualConvBlock(
+            in_channels=ch_layer_1, out_channels=ch_layer_1, num_layers=3,
+            activation_type='silu', norm_type='group', norm_group=num_images,
+        )
+        self.conv_block_1 = ResidualConvBlock(
+            in_channels=ch_layer_1, out_channels=ch_layer_1, num_layers=2,
+            activation_type='silu', norm_type='layer',
+        )
+        self.conv_block_2 = ResidualConvBlock(
+            in_channels=ch_layer_1, out_channels=ch_layer_2, num_layers=2,
+            activation_type='sigmoid', norm_type='layer',
+        )
+
+    def forward(self, images: List[torch.Tensor]) -> torch.Tensor:
+        """
+        :param images: image sequence, shape [B, L, C, H, W]
+        :return: Difficult Zone, shape [B, C, H, W]
+        """
+
+        ##################################
+        ae_differences = []
+        for i in range(self.num_images):
+            ae_differences.append(cal_ae(images[:, i, ...], images[:, i - 1, ...]))
+
+        avg_ae_differences = torch.mean(torch.stack(ae_differences), dim=0)
+
+        ##################################
+
+        x = torch.cat(ae_differences, dim=1)
+        x = self.conv_block_0(x)
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+
+        difficult_zone = x * self.merge_ratio + avg_ae_differences
+
+        if self.final_transform == 'tanh':
+            return torch.tanh(difficult_zone)
+        elif self.final_transform == 'sigmoid':
+            return torch.sigmoid(difficult_zone)
+        else:
+            return difficult_zone
+
+
+@MODULES_REGISTRY.register()
+class DifficultZoneEstimatorV3pb(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            num_images: int,
+            merge_ratio: float = 1.0,
+            final_transform: str = 'none',
+    ):
+        super().__init__()
+
+        self.channels = channels
+        self.num_images = num_images
+
+        self.merge_ratio = merge_ratio
+        self.final_transform = final_transform
+
+        from .residual_conv_block import ResidualConvBlock
+        diff_metric_methods = 2
+        ch_layer_0 = channels * num_images * diff_metric_methods
+        ch_layer_1 = channels * num_images
+        ch_layer_2 = channels
+        self.conv_block_0 = ResidualConvBlock(
+            in_channels=ch_layer_0, out_channels=ch_layer_0, num_layers=3,
+            activation_type='silu', norm_type='group', norm_group=num_images * diff_metric_methods,
+        )
+        self.conv_block_1 = ResidualConvBlock(
+            in_channels=ch_layer_0, out_channels=ch_layer_1, num_layers=2,
+            activation_type='silu', norm_type='layer',
+        )
+        self.conv_block_2 = ResidualConvBlock(
+            in_channels=ch_layer_1, out_channels=ch_layer_2, num_layers=2,
+            activation_type='sigmoid', norm_type='layer',
+        )
+
+    def _calculate_difference(self, images: List[torch.Tensor]) -> Tuple[List[torch.Tensor], List[torch.Tensor]]:
+        ae_differences = [] # L * [B, C, H, W]
+        bce_differences = [] # L * [B, C, H, W]
+        for i in range(self.num_images):
+            img1 = images[:, i, ...]
+            img2 = images[:, i - 1, ...]
+            ae_differences.append(
+                cal_ae(img1, img2)
+            )
+            bce_differences.append(
+                torch.sigmoid(cal_bce(img1, img2))
+            )
+        return ae_differences, bce_differences
+
+    def forward(self, images: List[torch.Tensor]) -> torch.Tensor:
+        """
+        :param images: image sequence, shape [B, L, C, H, W]
+        :return: Difficult Zone, shape [B, C, H, W]
+        """
+
+        ##################################
+        # L * [B, C, H, W], L * [B, C, H, W]
+        ae_differences, bce_differences = self._calculate_difference(images)
+
+        avg_ae_differences = torch.mean(torch.stack(ae_differences), dim=0)
+
+        ae_differences = torch.cat(ae_differences, dim=1)
+        bce_differences = torch.cat(bce_differences, dim=1)
+        differences = torch.cat((ae_differences, bce_differences), dim=1)
+
+        ##################################
+
+        x = differences
+        x = self.conv_block_0(x)
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+
+        difficult_zone = x * self.merge_ratio + avg_ae_differences
+
+        if self.final_transform == 'tanh':
+            return torch.tanh(difficult_zone)
+        elif self.final_transform == 'sigmoid':
+            return torch.sigmoid(difficult_zone)
+        else:
+            return difficult_zone
