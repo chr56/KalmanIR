7df1b5473d49f7484ba5dcf0a9b3c0eee0edff2c
chr56 <->
2025-12-02 21:31:48 +0800

    arch: enhance `ResidualConvBlock`

--- basicsr/archs/modules/utils.py
+++ basicsr/archs/modules/utils.py
@@ -2,7 +2,7 @@ from typing import Literal, Union, List, TypeVar
 
 from torch import nn
 
-ActivationFunction = Literal['relu', 'leaky_relu', 'silu', 'sigmoid']
+ActivationFunction = Literal['relu', 'leaky_relu', 'gelu', 'silu', 'sigmoid', 'tanh']
 
 
 def get_activation_function(activation_type, required: bool = True, **kwargs):
@@ -12,8 +12,12 @@ def get_activation_function(activation_type, required: bool = True, **kwargs):
         return nn.LeakyReLU(inplace=True, **kwargs)
     elif activation_type == 'silu':
         return nn.SiLU(inplace=True)
+    elif activation_type == 'gelu':
+        return nn.GELU()
     elif activation_type == 'sigmoid':
         return nn.Sigmoid()
+    elif activation_type == 'tanh':
+        return nn.Tanh()
     else:
         if required:
             raise NotImplementedError(f"activation type `{activation_type}` is unimplemented")
@@ -21,7 +25,7 @@ def get_activation_function(activation_type, required: bool = True, **kwargs):
             return nn.Identity()
 
 
-NormLayerType = Literal['batch', 'layer', 'group', 'none']
+NormLayerType = Literal['batch', 'layer', 'group', 'instance', 'none']
 
 
 def get_norm_layer(norm_type: NormLayerType, channel: int, norm_groups: int = -1) -> nn.Module:
@@ -33,6 +37,8 @@ def get_norm_layer(norm_type: NormLayerType, channel: int, norm_groups: int = -1
     elif norm_type == 'group':
         if norm_groups < 1: raise ValueError("norm_groups must be at least 1 for GroupNorm.")
         return nn.GroupNorm(num_groups=norm_groups, num_channels=channel, eps=1e-6)
+    elif norm_type == 'instance':
+        return nn.InstanceNorm2d(channel, affine=True, eps=1e-6)
     elif norm_type == 'none':
         return nn.Identity()
     else:
