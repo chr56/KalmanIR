dd06da5cdaa9be6140b3d6fa23c825eef595de19
chr56 <->
2025-12-19 11:53:05 +0800

    arch: add `EnhancedKalmanPredictorMambaBlockV1a`

--- basicsr/archs/modules/KalmanPredictorEnhanced_module.py
+++ basicsr/archs/modules/KalmanPredictorEnhanced_module.py
@@ -79,3 +79,88 @@ class EnhancedKalmanPredictorMambaBlockV0(nn.Module):
     @staticmethod
     def model_input_format():
         return ['image', 'difficult_zone']
+
+
+@MODULES_REGISTRY.register()
+class EnhancedKalmanPredictorMambaBlockV1a(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            num_images: int,
+            hidden_channel_expand: int = 24,
+            num_layer: int = 2,
+            final_activation: str = 'tanh',
+            mamba_d_state: int = 8,
+            mamba_d_expand: float = 2,
+            mamba_dt_rank: int = 'auto',
+            cab_compress_factor: int = 2,
+            cab_squeeze_factor: int = 4,
+            skip_weight_cca: float = 0.4,
+            skip_weight_bnc: float = 1.0,
+    ):
+        super().__init__()
+
+        self.channels = channels
+        self.num_images = num_images
+
+        from .residual_conv_block import ResidualConvBlock
+        self.dim_expand = self.channels * hidden_channel_expand
+        self.conv_block_expand_img = ResidualConvBlock(
+            self.channels * 2, self.dim_expand,
+            activation_type='tanh'
+        )
+        self.conv_block_expand_dz = ResidualConvBlock(
+            self.channels, self.dim_expand,
+            activation_type='leaky_relu', norm_type='instance'
+        )
+        self.conv_shrink = nn.Conv2d(
+            self.dim_expand, self.channels, kernel_size=3, padding='same'
+        )
+        if final_activation == 'sigmoid':
+            self.final_activation = nn.Sigmoid()
+        elif final_activation == 'tanh':
+            self.final_activation = nn.Tanh()
+        elif final_activation == 'none':
+            self.final_activation = nn.Identity()
+        else:
+            raise NotImplementedError(f'Activation {final_activation} not implemented')
+
+        from .modules_mamba_block import GuidedMambaVSSBlock
+        compressed_channels = self.dim_expand // cab_compress_factor
+        squeezed_channels = self.channels // cab_squeeze_factor
+
+        self.num_layer = num_layer
+        self.layers = nn.ModuleList()
+        for _ in range(num_layer):
+            self.layers.append(
+                GuidedMambaVSSBlock(
+                    dim=self.dim_expand,
+                    mamba_d_state=mamba_d_state,
+                    mamba_d_expand=mamba_d_expand,
+                    mamba_dt_rank=mamba_dt_rank,
+                    bnc_compressed_channel=compressed_channels,
+                    cca_squeezed_channel=squeezed_channels,
+                    cca_leaky_relu=1e-2,
+                    skip_weight_cca=skip_weight_cca,
+                    skip_weight_bnc=skip_weight_bnc,
+                )
+            )
+
+    def forward(self, image: torch.Tensor, difficult_zone: torch.Tensor) -> torch.Tensor:
+        # image; [B, C, H, W]
+        # difficult_zone: [B, C, H, W]
+
+        x = self.conv_block_expand_img(torch.cat([image, difficult_zone.detach()], dim=1))  # [B, C', H, W]
+        y = self.conv_block_expand_dz(difficult_zone)  # [B, C', H, W]
+
+        for layer in self.layers:
+            x = layer(x, y)
+
+        x = self.final_activation(self.conv_shrink(x))
+
+        # [B, C, H, W]
+        return x
+
+    @staticmethod
+    def model_input_format():
+        return ['image', 'difficult_zone']
