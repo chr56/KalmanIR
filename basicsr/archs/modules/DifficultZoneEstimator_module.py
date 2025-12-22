da29608b1879b527c3705641ce6b845e50a322e9
chr56 <->
2025-12-22 14:20:23 +0800

    arch: add `DifficultZoneEstimatorV5base`

--- basicsr/archs/modules/DifficultZoneEstimator_module.py
+++ basicsr/archs/modules/DifficultZoneEstimator_module.py
@@ -344,3 +344,94 @@ class DifficultZoneEstimatorV4dl(nn.Module):
         difficult_zone = dz_branch_dl
 
         return self.final_transform(difficult_zone)
+
+
+@MODULES_REGISTRY.register()
+class DifficultZoneEstimatorV5base(nn.Module):
+    def __init__(
+            self,
+            channels: int,
+            num_images: int,
+            ae_amplify: float = 4.,
+            bce_scale: float = 0.33,
+            expand: int = 2,
+            final_transform: str = 'none',
+    ):
+        super().__init__()
+
+        self.channels = channels
+        self.num_images = num_images
+
+        self.ae_amplify = ae_amplify
+        self.bce_scale = bce_scale
+
+        self.final_transform = _get_transform_layer(final_transform)
+
+        from .residual_conv_block import ResidualConvBlock
+        self.diff_methods = 2
+        self.expand = expand
+        ch_in = self.diff_methods * self.num_images * self.channels
+        ch_mid = self.diff_methods * self.num_images * self.channels * self.expand
+        self.conv_expand = nn.Conv2d(
+            in_channels=ch_in, out_channels=ch_mid, groups=self.diff_methods, kernel_size=3, padding='same',
+        )
+        self.activation_in = torch.nn.Sigmoid()
+        self.conv_block_a = ResidualConvBlock(
+            in_channels=ch_mid, out_channels=ch_mid,
+            activation_type='silu', norm_type='instance',
+        )
+        self.conv_block_b = ResidualConvBlock(
+            in_channels=ch_mid, out_channels=ch_mid,
+            activation_type='silu', norm_type='instance',
+        )
+        self.conv_shrink = nn.Conv2d(
+            in_channels=ch_mid, out_channels=self.channels, kernel_size=1,
+        )
+
+    def _calculate_difference(self, images: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        """
+        :param images: sequence of images [B, L, C, H, W]
+        :return: tuple of difference tensors [B, L, C, H, W]
+        """
+
+        images_shifted = torch.roll(images, shifts=1, dims=1)  # [B, L, C, H, W] (shifted in L)
+
+        ae_diff = cal_ae(images, images_shifted)  # [B, L, C, H, W]
+        bce_diff = cal_bce_sigmoid(images, images_shifted)  # [B, L, C, H, W]
+
+        return ae_diff, bce_diff
+
+    def _forward_differences(self, ae_differences: torch.Tensor, bce_differences: torch.Tensor):
+        """
+        :param ae_differences: [B, L, C, H, W]
+        :param bce_differences: [B, L, C, H, W]
+        :return: dz prediction [B, C, H, W]
+        """
+        B, L, C, H, W = ae_differences.shape
+
+        ae_cat = torch.sigmoid(ae_differences.reshape(B, C * L, H, W) * self.ae_amplify)
+        bce_cat = torch.tanh(bce_differences.reshape(B, C * L, H, W) * self.bce_scale)
+
+        differences = torch.cat((ae_cat, bce_cat), dim=1)
+
+        x_in = self.activation_in(self.conv_expand(differences))  # [B, 2 * L * C * expand, H, W]
+
+        x = self.conv_block_a(x_in)
+        x = self.conv_block_b(x) + x_in
+
+        x = self.conv_shrink(x)
+
+        return x
+
+    def forward(self, images: torch.Tensor) -> torch.Tensor:
+        """
+        :param images: image sequence, list of [B, C, H, W] tensors
+        :return: Difficult Zone, shape [B, C, H, W]
+        """
+
+        # [B, L, C, H, W], [B, L, C, H, W]
+        delta_ae, delta_bce = self._calculate_difference(images)
+
+        difficult_zone = self._forward_differences(delta_ae, delta_bce)  # [B, C, H, W]
+
+        return self.final_transform(difficult_zone)
